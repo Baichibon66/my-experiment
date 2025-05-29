e167c47c0081cc94247b853dd6e121b4dc9ac6b9
Baichibon66 <myf1440333982@163.com>
2025-05-29 09:23:43 +0800

    formal.js

--- formal.js
+++ formal.js
@@ -6,7 +6,7 @@ function getProlificPID() {
 const prolificPID = getProlificPID();
 console.log('Prolific ID:', prolificPID);
 
-const PROLIFIC_COMPLETION_URL = "https://app.prolific.com/submissions/complete?cc=C1M6ISR3"; // Completion Code
+const PROLIFIC_COMPLETION_URL = "https://app.prolific.com/submissions/complete?cc=C16PWI5X"; // Completion Code
 
 // ========== 1. パス設定 ==========
 const IMAGE_PATH = "formalimages/"; // images folder
