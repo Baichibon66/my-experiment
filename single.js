33e4ea69c7d1f56f906c0d3bc7131ab5315ad990
Baichibon66 <myf1440333982@163.com>
2025-06-01 12:05:55 +0800

    single.js

--- single.js
+++ single.js
@@ -116,7 +116,7 @@ function startExperiment() {
   });
 
   // ========== 画面4：主体実験の流れ ==========
-  for (let i = 0; i < 2; i++) {          //修改試行数
+  for (let i = 0; i < 5; i++) {          //修改試行数
     const trial = trials[i % trials.length];
     // ====== 被験者試行 ======
     // 画面3：刺激画面
